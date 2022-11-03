384088450ed5ce44531a8cdb4e0d266f943aeedf
Christian Sunday <potentialsunny47@gmail.com>
2022-11-03 02:17:09 +0100

    footer, carousel for reviews all in home page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
 import Header from './components/head';
 
 
@@ -12,7 +12,7 @@ function App() {
       <main className="mt-3">
         <div className="container">
           {/* {latest product section} */}
-          <h3 className="mb-4">Latest Products <a className="float-end btn btn-dark">View All Products <i class="fa-solid fa-arrow-right-long"></i></a> </h3> 
+          <h3 className="mb-4">Latest Products <a className="float-end btn btn-dark">View All Products <i className="fa-solid fa-arrow-right-long"></i></a> </h3> 
         <div className="row mb-4 ">
           {/* {Product Box} */}
             <div className="col-md-3 mb-3">
@@ -23,8 +23,8 @@ function App() {
                       <h5 className="card-title text-muted">Price: #. 500</h5>
                     </div>
                     <div className="card-footer">
-                        <button title="Add to Cart" className="btn btn-success btn-sm"><i class="fa-solid fa-cart-plus"></i></button>
-                        <button title="Add to Wishlist" className="btn btn-danger ms-1 btn-sm"><i class="fa fa-heart"></i></button>
+                        <button title="Add to Cart" className="btn btn-success btn-sm"><i className="fa-solid fa-cart-plus"></i></button>
+                        <button title="Add to Wishlist" className="btn btn-danger ms-1 btn-sm"><i className="fa fa-heart"></i></button>
                     </div>
                 </div>
             </div>
@@ -38,8 +38,8 @@ function App() {
                       <h5 className="card-title text-muted">Price: #. 500</h5>
                     </div>
                     <div className="card-footer">
-                        <button title="Add to Cart" className="btn btn-success btn-sm"><i class="fa-solid fa-cart-plus"></i></button>
-                        <button title="Add to Wishlist" className="btn btn-danger ms-1 btn-sm"><i class="fa fa-heart"></i></button>
+                        <button title="Add to Cart" className="btn btn-success btn-sm"><i className="fa-solid fa-cart-plus"></i></button>
+                        <button title="Add to Wishlist" className="btn btn-danger ms-1 btn-sm"><i className="fa fa-heart"></i></button>
                     </div>
                 </div>
             </div>
@@ -53,8 +53,8 @@ function App() {
                       <h5 className="card-title text-muted">Price: #. 500</h5>
                     </div>
                     <div className="card-footer">
-                        <button title="Add to Cart" className="btn btn-success btn-sm"><i class="fa-solid fa-cart-plus"></i></button>
-                        <button title="Add to Wishlist" className="btn btn-danger ms-1 btn-sm"><i class="fa fa-heart"></i></button>
+                        <button title="Add to Cart" className="btn btn-success btn-sm"><i className="fa-solid fa-cart-plus"></i></button>
+                        <button title="Add to Wishlist" className="btn btn-danger ms-1 btn-sm"><i className="fa fa-heart"></i></button>
                     </div>
                 </div>
             </div>
@@ -68,8 +68,8 @@ function App() {
                       <h5 className="card-title text-muted">Price: #. 500</h5>
                     </div>
                     <div className="card-footer">
-                        <button title="Add to Cart" className="btn btn-success btn-sm"><i class="fa-solid fa-cart-plus"></i></button>
-                        <button title="Add to Wishlist" className="btn btn-danger ms-1 btn-sm"><i class="fa fa-heart"></i></button>
+                        <button title="Add to Cart" className="btn btn-success btn-sm"><i className="fa-solid fa-cart-plus"></i></button>
+                        <button title="Add to Wishlist" className="btn btn-danger ms-1 btn-sm"><i className="fa fa-heart"></i></button>
                     </div>
                 </div>
             </div>
@@ -83,8 +83,8 @@ function App() {
                       <h5 className="card-title text-muted">Price: #. 500</h5>
                     </div>
                     <div className="card-footer">
-                        <button title="Add to Cart" className="btn btn-success btn-sm"><i class="fa-solid fa-cart-plus"></i></button>
-                        <button title="Add to Wishlist" className="btn btn-danger ms-1 btn-sm"><i class="fa fa-heart"></i></button>
+                        <button title="Add to Cart" className="btn btn-success btn-sm"><i className="fa-solid fa-cart-plus"></i></button>
+                        <button title="Add to Wishlist" className="btn btn-danger ms-1 btn-sm"><i className="fa fa-heart"></i></button>
                     </div>
                 </div>
             </div>
@@ -98,8 +98,8 @@ function App() {
                       <h5 className="card-title text-muted">Price: #. 500</h5>
                     </div>
                     <div className="card-footer">
-                        <button title="Add to Cart" className="btn btn-success btn-sm"><i class="fa-solid fa-cart-plus"></i></button>
-                        <button title="Add to Wishlist" className="btn btn-danger ms-1 btn-sm"><i class="fa fa-heart"></i></button>
+                        <button title="Add to Cart" className="btn btn-success btn-sm"><i className="fa-solid fa-cart-plus"></i></button>
+                        <button title="Add to Wishlist" className="btn btn-danger ms-1 btn-sm"><i className="fa fa-heart"></i></button>
                     </div>
                 </div>
             </div>
@@ -113,8 +113,8 @@ function App() {
                       <h5 className="card-title text-muted">Price: #. 500</h5>
                     </div>
                     <div className="card-footer">
-                        <button title="Add to Cart" className="btn btn-success btn-sm"><i class="fa-solid fa-cart-plus"></i></button>
-                        <button title="Add to Wishlist" className="btn btn-danger ms-1 btn-sm"><i class="fa fa-heart"></i></button>
+                        <button title="Add to Cart" className="btn btn-success btn-sm"><i className="fa-solid fa-cart-plus"></i></button>
+                        <button title="Add to Wishlist" className="btn btn-danger ms-1 btn-sm"><i className="fa fa-heart"></i></button>
                     </div>
                 </div>
             </div>
@@ -128,8 +128,8 @@ function App() {
                       <h5 className="card-title text-muted">Price: #. 500</h5>
                     </div>
                     <div className="card-footer">
-                        <button title="Add to Cart" className="btn btn-success btn-sm"><i class="fa-solid fa-cart-plus"></i></button>
-                        <button title="Add to Wishlist" className="btn btn-danger ms-1 btn-sm"><i class="fa fa-heart"></i></button>
+                        <button title="Add to Cart" className="btn btn-success btn-sm"><i className="fa-solid fa-cart-plus"></i></button>
+                        <button title="Add to Wishlist" className="btn btn-danger ms-1 btn-sm"><i className="fa fa-heart"></i></button>
                     </div>
                 </div>
             </div>
@@ -139,7 +139,7 @@ function App() {
         {/* {end latest product section} */}
 
          {/* {Popular category section} */}
-         <h3 className="mb-4">Popular Categories <a href="#" className="float-end btn btn-dark">View All Categories <i class="fa-solid fa-arrow-right-long"></i></a> </h3> 
+         <h3 className="mb-4">Popular Categories <a href="#" className="float-end btn btn-dark">View All Categories <i className="fa-solid fa-arrow-right-long"></i></a> </h3> 
         <div className="row mb-4">
           {/* {category Box} */}
             <div className="col-md-3 mb-3">
@@ -199,7 +199,7 @@ function App() {
 
 
          {/* {Popular product section} */}
-         <h3 className="mb-4">Popular Products <a className="float-end btn btn-dark">View All Products <i class="fa-solid fa-arrow-right-long"></i></a> </h3> 
+         <h3 className="mb-4">Popular Products <a className="float-end btn btn-dark">View All Products <i className="fa-solid fa-arrow-right-long"></i></a> </h3> 
         <div className="row mb-4">
           {/* {Popular Product Box} */}
             <div className="col-md-3 mb-3">
@@ -210,8 +210,8 @@ function App() {
                       <h5 className="card-title text-muted">Price: #. 500</h5>
                     </div>
                     <div className="card-footer">
-                        <button title="Add to Cart" className="btn btn-success btn-sm"><i class="fa-solid fa-cart-plus"></i></button>
-                        <button title="Add to Wishlist" className="btn btn-danger ms-1 btn-sm"><i class="fa fa-heart"></i></button>
+                        <button title="Add to Cart" className="btn btn-success btn-sm"><i className="fa-solid fa-cart-plus"></i></button>
+                        <button title="Add to Wishlist" className="btn btn-danger ms-1 btn-sm"><i className="fa fa-heart"></i></button>
                     </div>
                 </div>
             </div>
@@ -225,8 +225,8 @@ function App() {
                       <h5 className="card-title text-muted">Price: #. 500</h5>
                     </div>
                     <div className="card-footer">
-                        <button title="Add to Cart" className="btn btn-success btn-sm"><i class="fa-solid fa-cart-plus"></i></button>
-                        <button title="Add to Wishlist" className="btn btn-danger ms-1 btn-sm"><i class="fa fa-heart"></i></button>
+                        <button title="Add to Cart" className="btn btn-success btn-sm"><i className="fa-solid fa-cart-plus"></i></button>
+                        <button title="Add to Wishlist" className="btn btn-danger ms-1 btn-sm"><i className="fa fa-heart"></i></button>
                     </div>
                 </div>
             </div>
@@ -240,8 +240,8 @@ function App() {
                       <h5 className="card-title text-muted">Price: #. 500</h5>
                     </div>
                     <div className="card-footer">
-                        <button title="Add to Cart" className="btn btn-success btn-sm"><i class="fa-solid fa-cart-plus"></i></button>
-                        <button title="Add to Wishlist" className="btn btn-danger ms-1 btn-sm"><i class="fa fa-heart"></i></button>
+                        <button title="Add to Cart" className="btn btn-success btn-sm"><i className="fa-solid fa-cart-plus"></i></button>
+                        <button title="Add to Wishlist" className="btn btn-danger ms-1 btn-sm"><i className="fa fa-heart"></i></button>
                     </div>
                 </div>
             </div>
@@ -255,8 +255,8 @@ function App() {
                       <h5 className="card-title text-muted">Price: #. 500</h5>
                     </div>
                     <div className="card-footer">
-                        <button title="Add to Cart" className="btn btn-success btn-sm"><i class="fa-solid fa-cart-plus"></i></button>
-                        <button title="Add to Wishlist" className="btn btn-danger ms-1 btn-sm"><i class="fa fa-heart"></i></button>
+                        <button title="Add to Cart" className="btn btn-success btn-sm"><i className="fa-solid fa-cart-plus"></i></button>
+                        <button title="Add to Wishlist" className="btn btn-danger ms-1 btn-sm"><i className="fa fa-heart"></i></button>
                     </div>
                 </div>
             </div>
@@ -267,7 +267,7 @@ function App() {
 
 
            {/* {Popular seller section} */}
-           <h3 className="mb-4">Popular Sellers <a href="#" className="float-end btn btn-dark">View All Sellers <i class="fa-solid fa-arrow-right-long"></i></a> </h3> 
+           <h3 className="mb-4">Popular Sellers <a href="#" className="float-end btn btn-dark">View All Sellers <i className="fa-solid fa-arrow-right-long"></i></a> </h3> 
         <div className="row mb-4">
          {/* {seller Box} */}
          <div className="col-md-3 mb-3">
@@ -328,6 +328,81 @@ function App() {
         </div>
         {/* {end popular seller section} */}
 
+         {/* {start rating and reviews} */}
+         <div id="carouselExampleIndicators" className="carousel slide my-4 border bg-dark text-white p-5" data-bs-ride="true">
+                  <div className="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                  </div>
+                  <div className="carousel-inner">
+                    <div className="carousel-item active">
+                          <figure className="text-center">
+                              <blockquote className="blockquote">
+                                <p>A well-known quote, contained in a blockquote element.</p>
+                              </blockquote>
+                              <figcaption className="blockquote-footer">
+                                <i className='fa fa-star text-warning'></i>
+                                <i className='fa fa-star text-warning'></i>
+                                <i className='fa fa-star text-warning'></i>
+                                <i className='fa fa-star text-warning'></i>
+                                <cite title="Source Title">Customer Name</cite>
+                              </figcaption>
+                          </figure>
+                    </div>
+                    <div className="carousel-item">
+                          <figure className="text-center">
+                              <blockquote className="blockquote">
+                                <p>A well-known quote, contained in a blockquote element.</p>
+                              </blockquote>
+                              <figcaption className="blockquote-footer">
+                                <i className='fa fa-star text-warning'></i>
+                                <i className='fa fa-star text-warning'></i>
+                                <i className='fa fa-star text-warning'></i>
+                                <cite title="Source Title">Customer Name</cite>
+                              </figcaption>
+                          </figure>
+                    </div>
+                    <div className="carousel-item">
+                          <figure className="text-center">
+                              <blockquote className="blockquote">
+                                <p>A well-known quote, contained in a blockquote element.</p>
+                              </blockquote>
+                              <figcaption className="blockquote-footer">
+                                <i className='fa fa-star text-warning'></i>
+                                <i className='fa fa-star text-warning'></i>
+                                <cite title="Source Title">Customer Name</cite>
+                              </figcaption>
+                          </figure>
+                    </div>
+                  </div>
+                  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                  </button>
+                  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                  </button>
+          </div>
+
+         {/* {end rating and reviews} */}
+         {/* {footer start} */}
+         <footer class="d-flex flex-wrap justify-content-between align-items-center my-4 border-top">
+              <div class="col-md-4 d-flex align-items-center">
+                <a href="/" class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1">
+                         
+                </a>
+                <span class="mb-3 mb-md-0 text-muted">© 2022 Company, Inc</span>
+              </div>
+
+              <ul class="nav col-md-4 justify-content-end list-unstyled d-flex pt-2">
+                <li class="ms-3"><a class="text-muted" href="#"> <i className='fa-brands fa-facebook fa-2x'></i></a></li>
+                <li class="ms-3"><a class="text-muted" href="#"><i class="fa-brands fa-twitter fa-2x"></i></a></li>
+                <li class="ms-3"><a class="text-muted" href="#"><i class="fa-brands fa-instagram fa-2x"></i></a></li>
+              </ul>
+          </footer>
+         {/* {footer ends} */}
         </div>
       </main>
     </>  
